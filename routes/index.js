354adec26fa092d206666921b6076b7df750a320
adityaj56 <adityajangid2015@gmail.com>
2023-03-20 19:43:18 +0530

    new jobs api added

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,17 @@ const loginController = require('../controllers/login_controller');
 router.get('/', loginController.loginPage);
 router.get('/sign-up', loginController.signupPage);
 router.get('/logout', loginController.logout);
+router.get('/new-openings', function(req, res){
+    return res.render('newOpenings',{
+        title: 'new openings',
+        layout: 'layouts/layout2'
+    })
+});
 
 router.use('/user', require('./users'));
 router.use('/student', require('./students'));
 router.use('/interview', require('./interviews'));
+
+
 
 module.exports = router;
