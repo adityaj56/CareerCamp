541fd536aa0ee58a01bdc5fb8ab6e78a823cbc48
adityaj56 <adityajangid2015@gmail.com>
2023-03-20 14:39:47 +0530

    added authentication for requests

--- routes/interviews.js
+++ routes/interviews.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport')
 
 const interviewController = require('../controllers/interview_controller')
 
-router.get('/', interviewController.page);
+router.get('/', passport.checkAuthentication, interviewController.page);
 
-router.post('/create', interviewController.create);
-router.post('/add-student', interviewController.addStudent);
-router.post('/update-student-status', interviewController.updateStudentStatus);
+router.post('/create', passport.checkAuthentication, interviewController.create);
+router.post('/add-student', passport.checkAuthentication, interviewController.addStudent);
+router.post('/update-student-status', passport.checkAuthentication, interviewController.updateStudentStatus);
 
 module.exports = router;
--- routes/students.js
+++ routes/students.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router()
+const passport = require('passport');
 
 const studentController = require('../controllers/student_controller');
 
-router.post('/create', studentController.create);
-router.get('/download-csv', studentController.downloadCSV);
+router.post('/create', passport.checkAuthentication, studentController.create);
+router.get('/download-csv', passport.checkAuthentication, studentController.downloadCSV);
 
 module.exports = router;
